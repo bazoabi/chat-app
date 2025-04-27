71078f1d321bd40758d3e539628ff77d38b11d21
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-28 00:54:54 +0200

    Installing CORS for backend via NPM

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +14,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // Enable CORS with credentials
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
