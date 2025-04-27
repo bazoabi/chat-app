885f5a84904c29005f37f1cd2139e0a1a8565352
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-28 01:21:12 +0200

    Protect Routes By Navigating To The Proper Page Based On User Auth Status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,54 @@ import SettingsPage from "./pages/SettingsPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+
+import { Loader } from "lucide-react";
+
+import { Navigate } from "react-router-dom";
+
 // This is the main component of the chat app.
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    // Check if the user is authenticated when the app loads
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("Auth User:", authUser);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="animate-spin" size={32} />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
