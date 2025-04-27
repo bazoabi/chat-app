139be5e637367eb4515b4a1d2632f16cb4f7610e
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-27 17:06:49 +0200

    Remove unnecessary verifyToken logic, and fix response of the signUP logic

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,18 +14,3 @@ export const generateToken = (userId, res) => {
 
   return token;
 };
-
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token; // Get the token from cookies
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: "Forbidden" });
-    }
-    req.userId = decoded.id; // Attach user ID to request object
-    next(); // Proceed to the next middleware or route handler
-  });
-};
