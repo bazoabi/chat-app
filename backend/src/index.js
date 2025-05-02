144e48b1e343c7127f8c1d4f01532b560b5a0e26
Bader Zoabi <bader.zoabi@gmail.com>
2025-05-02 04:01:41 +0200

    Update Packages and Build Commands For Prod Publishing

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +12,7 @@ import { app, server } from "./lib/socket.js"; // Import the socket.io instance
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve(); // Get the current directory name
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
@@ -17,6 +20,13 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // Enable 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // Serve static files from the frontend build directory
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); // Serve the index.html file for all other routes
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
