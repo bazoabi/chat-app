5a2b9919bf0ccdda38d991b402343bcc740847e5
Bader Zoabi <bader.zoabi@gmail.com>
2025-05-02 04:18:46 +0200

    Apply fixed path instead of param for Renderer

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve(); // Get the current directory name
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); // Enable CORS with credentials
+app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Enable CORS with credentials
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
